8082aa213b745bca51c03e29eeb0ef8196fbe5b1
ambateman <tmbateman@hotmail.com>
2017-10-11 20:07:14 -0700

    PostalChallenge

---
+++ ChallengePostalCalculator/default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ChallengePostalCalculator._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Postal Calculator</div>
+        <p>
+            &nbsp;Width:
+            <asp:TextBox ID="txtWidth" runat="server" AutoPostBack="True" OnTextChanged="txtWidth_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            Height:
+            <asp:TextBox ID="txtHeight" runat="server" AutoPostBack="True" OnTextChanged="txtHeight_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            Length:<asp:TextBox ID="txtLength" runat="server" AutoPostBack="True" OnTextChanged="TextBox3_TextChanged" Height="16px" Width="125px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:RadioButton ID="radGround" runat="server" AutoPostBack="True" Text="Ground" Checked="True" GroupName="post" OnCheckedChanged="radGround_CheckedChanged" />
+        </p>
+        <p>
+            <asp:RadioButton ID="radAir" runat="server" AutoPostBack="True" Text="Air" GroupName="post" OnCheckedChanged="radAir_CheckedChanged" />
+        </p>
+        <p>
+            <asp:RadioButton ID="radNextDay" runat="server" AutoPostBack="True" Text="Next Day" GroupName="post" OnCheckedChanged="radNextDay_CheckedChanged" />
+        </p>
+        <p>
+            <asp:Label ID="lblPostage" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculator/default.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ChallengePostalCalculator
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        private double parcelWidth = 0.00;
+        private double parcelHeight = 0.00;
+        private double parcelLength = 0.00;
+        private double totalPostage = 0.00;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {  
+            if (!IsPostBack)
+            {
+                ViewState.Add("parcelWidth", parcelWidth);
+                ViewState.Add("parcelHeight", parcelHeight);
+                ViewState.Add("parcelLength", parcelLength);
+            }
+ 
+        }
+
+
+     
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(txtLength.Text.ToString(), out parcelLength);
+            ViewState["parcelLength"] = parcelLength;
+            GetTotalPostage();
+        }
+  
+
+        protected void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(txtWidth.Text.ToString(), out parcelWidth);
+            ViewState["parcelWidth"] = parcelWidth;
+            GetTotalPostage();
+        }
+
+        protected void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+            double.TryParse(txtHeight.Text.ToString(), out parcelHeight);
+            ViewState["parcelHeight"] = parcelHeight;
+            GetTotalPostage();
+        }
+
+        //---------------------------------------------------------------------
+        // Methods
+        //---------------------------------------------------------------------
+
+        private double CalculateVolume(double dblWidth, double dblHeight)
+        {   
+            return dblWidth*dblHeight;
+        }
+
+        private double CalculateVolume(double dblWidth, double dblHeight, double dblLength)
+        {
+            return dblWidth * dblHeight * dblLength;
+        }
+
+        private double MailingCost(double Volume)
+        {
+            if (radAir.Checked) return .25 * Volume;
+            if (radGround.Checked)
+            { return .15 * Volume;
+            }
+            else
+            { return .45 * Volume;
+            }
+  
+        }
+
+        private void GetTotalPostage()
+        {
+            totalPostage = 0.00;  //Initialize the postage value
+
+            parcelWidth = (double)ViewState["parcelWidth"];
+            parcelHeight = (double)ViewState["parcelHeight"];
+            parcelLength = (double)ViewState["parcelLength"];
+
+            if (parcelWidth > 0 && parcelHeight > 0 && parcelLength > 0)
+            {
+                totalPostage = MailingCost(CalculateVolume(parcelWidth, parcelHeight, parcelLength));
+            }
+            else
+            {
+                if (parcelHeight > 0 && parcelWidth > 0) totalPostage = MailingCost(CalculateVolume(parcelWidth, parcelHeight));
+            }
+            if (totalPostage > 0)
+            {
+                lblPostage.Text =  String.Format("Your parcel will cost {0:C}", totalPostage) + " to ship.";
+            }
+            else
+            {
+                lblPostage.Text = "";
+            }
+        }
+
+        protected void radGround_CheckedChanged(object sender, EventArgs e)
+        {
+            GetTotalPostage();
+        }
+
+        protected void radAir_CheckedChanged(object sender, EventArgs e)
+        {
+            GetTotalPostage();
+        }
+
+        protected void radNextDay_CheckedChanged(object sender, EventArgs e)
+        {
+            GetTotalPostage();
+        }
+    }
+}
